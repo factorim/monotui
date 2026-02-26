a7c857952f20693be57c825a5fc7ce272e23ff07
phoax <factorimio@gmail.com>
2026-02-26 21:11:05 +0100

    feat: add responsive box

--- src/components/features/WorkspaceGrid/WorkspaceGrid.tsx
+++ src/components/features/WorkspaceGrid/WorkspaceGrid.tsx
@@ -8,6 +8,7 @@ import type { GridTheme } from "../../../theme/theme.js"
 import { getQuickActionColumnMaxSizes } from "../../../utils/navigation/navigation-helper.js"
 import { getWorkspaceQuickActionCells } from "../../../utils/workspace/workspace-grid.js"
 import { Notification } from "../../ui/Notification.js"
+import { ResponsiveBox } from "../../ui/ResponsiveBox.js"
 import { WorkspaceRow } from "./WorkspaceRow.js"
 
 export function WorkspaceGrid() {
@@ -31,21 +32,21 @@ export function WorkspaceGrid() {
     >
       <Box flexDirection="column" flexGrow={1}>
         <Box width="100%" borderColor="gray">
-          <Box width="24%">
+          <ResponsiveBox width={{ s: "25%", m: "25%", l: "24%", xl: "24%" }}>
             <Text {...styles.headerText()}>Name{width}</Text>
-          </Box>
-          <Box width="10%">
+          </ResponsiveBox>
+          <ResponsiveBox width={{ s: null, m: "15%", l: "14%", xl: "18%" }}>
             <Text {...styles.headerText()}>Type</Text>
-          </Box>
-          <Box width="20%">
+          </ResponsiveBox>
+          <ResponsiveBox width={{ s: null, m: null, l: "24%", xl: "28%" }}>
             <Text {...styles.headerText()}>Path</Text>
-          </Box>
-          <Box width="20%">
+          </ResponsiveBox>
+          <ResponsiveBox width={{ s: "30%", m: "25%", l: "24%", xl: "28%" }}>
             <Text {...styles.headerText()}>Quick Actions</Text>
-          </Box>
-          <Box width="23%">
+          </ResponsiveBox>
+          <ResponsiveBox width={{ s: "45%", m: "35%", l: "23%", xl: "23%" }}>
             <Text {...styles.headerText()}>State</Text>
-          </Box>
+          </ResponsiveBox>
         </Box>
         {workspacesNavigationGrid.map((row, rowNb) => (
           <WorkspaceRow
